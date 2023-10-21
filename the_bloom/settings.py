46885239427899bdea6c205c116e7303a2b12d4b
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-10-21 19:44:33 +0000

    Add header, home page and css

--- the_bloom/settings.py
+++ the_bloom/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
     },
 ]
 
-AUTHENTIFICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = (
 
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
@@ -150,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
