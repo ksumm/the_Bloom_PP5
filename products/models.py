631064f9370594044357fb17b8943ee9aab1c2e1
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-10-25 11:01:26 +0000

    Add products app and models

---
+++ products/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def change_in_stock(self):
+        """ automatically change stock label """
+        if self.count == 0:
+            self.in_stock = False
+        else:
+            self.in_stock = True
+        self.save()    
