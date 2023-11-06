8a557d0ce8003ebc9fe57b42775ed4cf9734d157
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-11-06 20:09:26 +0000

    Add order history and update checkout view

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
@@ -17,7 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'delivery_cost', 'order_total',
                        'grand_total', 'original_bag', 'stripe_pid', )
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
