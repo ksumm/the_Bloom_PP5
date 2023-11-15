5ce195c99bdb96d923afe2fb167ab382fff61b15
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-11-15 23:54:08 +0000

    Add Delivery Info page

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path(
+        'delivery/',
+        views.delivery,
+        name='delivery'
+        ),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def delivery(request):
+    """ A view to return the index page and products"""
+    return render(request, "home/delivery.html")    
