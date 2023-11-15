98cb74b10c60810a57d3c349c65362aef9bab9a0
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-11-15 22:20:27 +0000

    Complete Contact Us form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact_view(request):
-    return HttpResponse("Contact app works!")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, f'The Contact Form was sent successfully!')
+            return render(request, 'contact/contact_success.html')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
