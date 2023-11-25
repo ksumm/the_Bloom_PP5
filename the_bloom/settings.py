1ea19841e6993aadcfbda4bbfec8a6bd9bd7e9c3
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-11-25 16:04:49 +0000

    Start deployment

--- the_bloom/settings.py
+++ the_bloom/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu105.gitpod.io', '8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu105.gitpod.io', '8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu106.gitpod.io', 'https://the-bloom-art-3c20c7ea337d.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu105.gitpod.io', 'https://8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu106.gitpod.io']
 
@@ -130,12 +131,19 @@ WSGI_APPLICATION = 'the_bloom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+
 
 
 # Password validation
