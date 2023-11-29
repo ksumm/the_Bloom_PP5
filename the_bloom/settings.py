24f7218d2c5ba58cc2a924a9282140c3e1b281b8
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-11-29 14:28:11 +0000

    Change debug

--- the_bloom/settings.py
+++ the_bloom/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ 
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu105.gitpod.io', '8000-ksumm-thebloompp5-8mfgsff0z1v.ws-eu106.gitpod.io', 'the-bloom-art-3c20c7ea337d.herokuapp.com', 'localhost',] # noqa
 
@@ -132,17 +132,6 @@ WSGI_APPLICATION = 'the_bloom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-#    DATABASES = {
-#        'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
 
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
